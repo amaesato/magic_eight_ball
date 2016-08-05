ea9d3c5c32be2f54e211cc0382f6c5c47d722c70
Ann Hall <annhall@Anns-MacBook-Pro.local>
2016-08-04 21:53:15 -0600

    final working code

--- magic_eight_ball.rb
+++ magic_eight_ball.rb
@@ -9,17 +9,13 @@
   "I don't care."
 ]
 
-@user_answer = []
-@my_questions = [
-
-]
 
 def question
   puts 'What is your question?'
   question = gets.strip.downcase
   result = @answer.sample
-  puts "Magic 8 Ball says: #{result}"
-  puts 'Ask me more and you will get a surprise'
+  puts "\nMagic 8 Ball says: #{result}"
+
 
   if question == "quit"
     exit(0)
@@ -29,7 +25,7 @@ def question
 end
 
 def surprise
-  puts 'I promised you a surprise'
+  puts "\nI promised you a surprise"
   puts 'Do you want to answer my questions now? y/n'
   reply1 = gets.strip.downcase
 
@@ -41,34 +37,31 @@ def surprise
   end
 end
 
+
+
 def question_for_user
-  puts 'What should I eat today?'
+
+  puts "\nWhat should I eat today?"
   user_input = gets.strip
-  
+  @user_input = @answer.clone
     if @answer.include?(user_input)
       puts "I don't like that answer, try again."
       question_for_user
+    elsif user_input == "quit"
+      exit(0)
     else
-      @user_answer << user_input
+      @user_input << user_input
+      question2
     end
-
-  puts 'Okay, Do I get rich?'
-  @user_answer << gets.strip
-  puts 'One more. Should I go to DPL today?'
-  @user_answer << gets.strip
-
-  if @user_answer == "quit"
-    exit
-  else
-    3.times {question2}
-  end
 end
 
+
 def question2
-  puts 'Now humer me. Give me another question:'
-  question = gets.strip.downcase
-  result = @user_answer.sample
+  puts 'Now, ask me a question:'
+  question = gets
+  result = @user_input.last
   puts "Magic 8 Ball says: #{result}"
+  puts "\nDid you see what just happened there?"
 
   if question == "quit"
     exit(0)
@@ -78,14 +71,42 @@ def question2
 end
 
 def other_options
-  puts 'Did you see what just happened there?'
+  puts "Would you like to do more?\t
+        1. Add more answers\t
+        2. Reset to the original answers\t
+        3. Show all possible answers\n"
+  option = gets.to_i
+  case option
+  when 0
+    puts 'Invalid option try again'
+    other_options
+  when 1 # needs to go somewhere
+    print "Answer: "
+    add = gets.strip
+
+      if @user_input.include?(add)
+        puts "That is already an answer."
+        other_options
+      elsif add == "quit"
+        exit(0)
+      else
+        @user_input << add
+        other_options
+      end
+  when 2
+    puts 'Ansers are reset. Goodbye.'
+    exit(0)
+  when 3
+    @user_input.each { |show| puts show }
+  end
 end
 
 
 
 
-
-puts 'I am Magic Eight Ball'
-3.times {question}
+puts "\nI am Magic Eight Ball\t
+      You can 'quit' anytime"
+question
+puts "\nAsk another question and you'll get a surprise"
+question
 surprise
-question_for_user
